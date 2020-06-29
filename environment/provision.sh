94f24f94b778707df885e857959764b7159abeef
nathanforester <nfeugene86@gmail.com>
2020-06-29 17:24:03 +0100

    first commit

---
+++ environment/provision.sh
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+
+sudo apt-get install nginx -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+sudo apt-get install -y nodejs
+
+npm install pm2 -g
+
+service nginx start
+
